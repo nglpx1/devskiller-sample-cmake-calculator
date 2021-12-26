269e5f751a453558735692e077d1112babf9d4a3
Angelo Paccosi <nglpx1@gmail.com>
2021-12-26 13:21:02 +0100

    Commit di prova

--- src/Calculator.cpp
+++ src/Calculator.cpp
@@ -1,17 +1,18 @@
 #include "Calculator.hpp"
 
 int Calculator::Add(int a, int b) {
-    return 0;
+
+    return (a+b);
 }
 
 int Calculator::Subtract(int a, int b) {
-    return 0;
+    return (a-b);
 }
 
 int Calculator::Multiply(int a, int b) {
-    return 0;
+    return (a*b);
 }
 
 double Calculator::Divide(int a, int b) {
-    return 0;
+    return (a/b);
 }
